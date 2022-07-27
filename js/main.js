cd5c24c31524772d77e2a1fee1b48b912c47393d
sadbc1235 <sadbc1235@naver.com>
2022-07-27 20:59:50 +0900

    add study content

--- js/main.js
+++ js/main.js
@@ -8,15 +8,16 @@ menuBtnBox.addEventListener("click", () => {
   }
 })
 
-const arrowRight = document.querySelector(".arrowRight");
-const arrowLeft = document.querySelector(".arrowLeft");
-const cardInner = document.querySelector(".cardInner");
+const toyProject = document.querySelector(".toyProject");
+const toyArrowRight = toyProject.querySelector(".arrowRight");
+const toyArrowLeft = toyProject.querySelector(".arrowLeft");
+const cardInner = toyProject.querySelector(".cardInner");
 const cards = cardInner.querySelectorAll(".card");
 
 cardInner.style.width = (cards.length * 250)+"px";
 let cardInnerLeft = 20;
 let index = 0;
-arrowRight.addEventListener("click", () => {
+toyArrowRight.addEventListener("click", () => {
   cardInnerLeft -= 250;
   cardInner.style.left = cardInnerLeft+"px";
   cards.forEach(card => {
@@ -28,14 +29,14 @@ arrowRight.addEventListener("click", () => {
     index = cards.length-1;
   }
   if (index == cards.length-1) {
-    arrowRight.classList.add("active");
+    toyArrowRight.classList.add("active");
   } else {
-    arrowRight.classList.remove("active");
-    arrowLeft.classList.remove("active");
+    toyArrowRight.classList.remove("active");
+    toyArrowLeft.classList.remove("active");
   }
 })
 
-arrowLeft.addEventListener("click", () => {
+toyArrowLeft.addEventListener("click", () => {
   cardInnerLeft += 250;
   cardInner.style.left = cardInnerLeft+"px";
 
@@ -48,9 +49,66 @@ arrowLeft.addEventListener("click", () => {
     index = 0;
   }
   if (index == 0) {
-    arrowLeft.classList.add("active");
+    toyArrowLeft.classList.add("active");
   } else {
-    arrowLeft.classList.remove("active");
-    arrowRight.classList.remove("active");
+    toyArrowLeft.classList.remove("active");
+    toyArrowRight.classList.remove("active");
   }
+})
+
+const study = document.querySelector(".study");
+const studyArrowLeft = study.querySelector(".arrowLeft");
+const studyArrowRight = study.querySelector(".arrowRight");
+const studyCardView = study.querySelector(".cardView");
+const studyCards = study.querySelectorAll(".card");
+
+studyCardView.style.width = (studyCards.length*210) + "px";
+
+let studyCardViewLeft = 30;
+let studyIndex = 0;
+
+studyArrowRight.addEventListener("click", () => {
+  studyCardViewLeft -= 210;
+  studyCardView.style.left = studyCardViewLeft+"px";
+
+  studyCards.forEach(studyCard => {
+    studyCard.classList.remove("module");
+    studyCard.classList.add("module--reverse");
+  })
+  studyIndex += 1;
+  studyCards[studyIndex].classList.add("module");
+  studyCards[studyIndex].classList.remove("module--reverse");
+  if(studyIndex == studyCards.length) {
+    studyIndex = studyCards.length-1;
+  }
+  if (studyIndex == studyCards.length-1) {
+    studyArrowRight.classList.add("active");
+  } else {
+    studyArrowRight.classList.remove("active");
+    studyArrowLeft.classList.remove("active");
+  }
+
+})
+
+studyArrowLeft.addEventListener("click", () => {
+  studyCardViewLeft += 210;
+  studyCardView.style.left = studyCardViewLeft+"px";
+
+  studyCards.forEach(studyCard => {
+    studyCard.classList.remove("module");
+    studyCard.classList.add("module--reverse");
+  })
+  studyIndex -= 1;
+  studyCards[studyIndex].classList.add("module");
+  studyCards[studyIndex].classList.remove("module--reverse");
+  if(studyIndex == -1) {
+    studyIndex = 0;
+  }
+  if (studyIndex == 0) {
+    studyArrowLeft.classList.add("active");
+  } else {
+    studyArrowLeft.classList.remove("active");
+    studyArrowRight.classList.remove("active");
+  }
+
 })
